27593509c0f5f85804fcd289ed4c0e62e76ce808
342624757@qq.com <342624757@qq.com>
2016-05-25 19:06:42 +0800

    level crossing runnable

--- levelCrossing.R
+++ levelCrossing.R
@@ -7,7 +7,7 @@ levelCrossing = function(d1, d2){
 
 generateTempBits = function(d,alpha = 0.2){
   len = nrow(d)
-  connectedData = rbind(d[,1],d[,2],d[,3])
+  connectedData = c(d[,1],d[,2],d[,3])
   meanData = mean(connectedData); sdData = sd(connectedData)
   q_plus = meanData + alpha*sdData; q_minus = meanData - alpha*sdData
   tempBits = ifelse(connectedData>q_plus,1,2)
@@ -18,16 +18,17 @@ generateTempBits = function(d,alpha = 0.2){
 bitsByCooperate = function(alice, bob){
   excurtionIndexesAlice = getExcurtionIndexes(alice)
   excurtionIndexesBob = getIndexesFromBob(excurtionIndexesAlice, bob)
-  return(list(a = alice[excurtionIndexesBob,], b= bob[excurtionIndexesBob,]))
+  return(list(a = alice[excurtionIndexesBob], b= bob[excurtionIndexesBob]))
 }
 
 getExcurtionIndexes = function(d, m=3){
   len = length(d)
   indexes = c()
+  preBit = d[1]
   for( i in 1:len ){
     cuBit = d[i]
     
-    if(cuBit == preBit && cuBit!=2) consecutive++
+    if(cuBit == preBit && cuBit!=2) consecutive = consecutive+1
     else {consecutive = 1; preBit = cuBit}
     
     if(consecutive == m){
@@ -38,10 +39,14 @@ getExcurtionIndexes = function(d, m=3){
   return(indexes)
 }
 
-getIndexesFromBob = function(excurtionIndexesAlice, tempBits){
+getIndexesFromBob = function(excurtionIndexesAlice, tempBits, m = 3){
+  len = length(excurtionIndexesAlice)
+  indexes = c()
   for(i in 1:len){
-    target = tempBits[i]
+    index = excurtionIndexesAlice[i]
+    target = tempBits[index]
     hasExcurtion = T
+    start = index-(m-1)/2; end = index+(m-1)/2
     
     for(j in start:end){
       if(tempBits[j]!=target || tempBits[j]==2){  hasExcurtion = F; break;  }
